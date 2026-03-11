45dbcdf6c1d46261b5f8773454a99b4d4aaff3e8
Blenda Almeida <blendaalmeida2008@hotmail.com>
2026-03-11 21:50:08 +0000

    Organize project structure

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import sys
+import os
 
 if len(sys.argv) < 2:
-    print("Usage: python3 clean_data.py input.csv")
+    print("Usage: python3 src/clean_data.py data/sample_data.csv")
     sys.exit()
 
 input_file = sys.argv[1]
@@ -13,7 +14,9 @@ df = df.drop_duplicates()
 df = df.dropna(how="all")
 df.columns = df.columns.str.strip().str.lower()
 
-output_file = "cleaned_" + input_file
+filename = os.path.basename(input_file)
+output_file = "cleaned_" + filename
+
 df.to_csv(output_file, index=False)
 
 print(f"Cleaned file saved as {output_file}")
